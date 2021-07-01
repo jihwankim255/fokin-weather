8b28459d78802845f0255a786ecbb0b613a72bda
devilish <plasticat104@gmail.com>
2021-07-01 17:58:30 +0900

    stye flex

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.yellowView}><Text>Hello</Text></View>
+      <View style={{flex: 1,backgroundColor: "blue"}}><Text>Hello</Text></View>
     </View>
   );
 }
@@ -15,7 +14,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  yellowView: {
+    flex:1,
+    backgroundColor:"yellow"
+  },
+  blueView: {
+    flex: 1,
+    backgroundColor: "blue"
+  }
 });
